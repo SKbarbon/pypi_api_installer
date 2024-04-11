7084023779b0f70ba2c9557420ace3b60bc361dc
Kot <86029286+SKbarbon@users.noreply.github.com>
2024-04-11 22:29:29 +0300

    use .gitignore file

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import requests, json
 
 
-URL = f"https://pypi.org/pypi/requests/json"
+URL = f"https://pypi.org/pypi/googletrans/json"
 
 r = requests.get(URL).text
 r = json.loads(r)
-
+print(r['info'])
 for i in r['info']['requires_dist']:
     print(i)
